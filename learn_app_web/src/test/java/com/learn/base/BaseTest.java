44d2bd823d9fe3d1d99256d9fbec23c5c3574032
ji_fei <jifei@souche.com>
2018-09-05 10:00:43 +0800

    no message

--- learn_app_web/src/test/java/com/learn/base/BaseTest.java
+++ learn_app_web/src/test/java/com/learn/base/BaseTest.java
@@ -2,8 +2,9 @@ package com.learn.base;
 
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(Junit4ClassRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:/configruation/spring/spring-main.xml", "classpath*:/configruation/spring/spring-mvc.xml"})
 //public class BaseTest extends AbstractTransactionalJUnit4SpringContextTests{
 public class BaseTest {
--- learn_app_web/src/test/java/com/learn/test/dao/PersonDaoTest.java
+++ learn_app_web/src/test/java/com/learn/test/dao/PersonDaoTest.java
@@ -1,21 +1,24 @@
 package com.learn.test.dao;
 
-import java.util.List;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+//import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.learn.base.BaseTest;
 import com.learn.dao.PersonDao;
-import com.learn.domain.PersonDO;
 
 public class PersonDaoTest extends BaseTest {
+	
+    public static Logger logger = LogManager.getLogger(PersonDaoTest.class);
 	
 	@Autowired
 	private PersonDao personDao;
 	@Test
 	public void testQuery() {
-		List<PersonDO> query = personDao.query();
-		System.out.println(query);
+		logger.info("aa【{}】{}{}{}{}{}{}{}{}{}{}{}", 12, 23,23,23,23,23,23,23,23,23,23,23,23);
+//		List<PersonDO> query = personDao.query();
+//		System.out.println(query);
 	}
 }
