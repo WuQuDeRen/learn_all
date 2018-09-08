5cf8588832366962435ff62f0aa5eeab18437fdd
ji_fei <jifei@souche.com>
2018-09-08 18:02:10 +0800

    no message

---
+++ learn_app_common/src/main/java/com/learn/system/bean/GenericBean.java
@@ -0,0 +1,9 @@
+package com.learn.system.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public abstract class GenericBean {
+	// @Lookup
+	// public abstract Object getObject();
+}
